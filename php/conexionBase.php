34b2fe94e622132ed9ebabe3602d9996e45f9c60
alexisvg2520 <78839355+alexisvg2520@users.noreply.github.com>
2021-12-02 17:57:08 -0500

    actualización instalación

--- php/conexionBase.php
+++ php/conexionBase.php
@@ -1,7 +1,6 @@
 <?php
-    include "..\php\db.inc.php";
+    include ("db.inc.php");
 
-             
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         // set the PDO error mode to exception
@@ -19,10 +18,12 @@
         )";
         // use exec() because no results are returned
         $conn->exec($sql);
-        echo "Table MyGuests created successfully";
+        echo "Creación de tabla Exitosa";
       } catch(PDOException $e) {
         echo $sql . "<br>" . $e->getMessage();
       }
     
       $conn = null;
+
+      
 ?>
